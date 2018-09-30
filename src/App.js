4c3753c79f7911d2fec30370aa6e56363d72756b
Yasin <yasincidem@gmail.com>
2018-09-30 14:52:50 +0300

    eslint added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react'
-import './App.css'
-import Button from '@material-ui/core/Button'
-import YourComponent from './Components/YourComponent'
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import YourComponent from './Components/YourComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Button variant="outlined">
-            Sample Button from App Component
-          </Button>
-          <Switch>
-            <Route path='/yourcomponent' component={YourComponent}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Button variant="outlined">
+        Sample Button from App Component
+      </Button>
+      <Switch>
+        <Route path="/yourcomponent" component={YourComponent} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
--- src/config/fire.js
+++ src/config/fire.js
@@ -1,15 +1,10 @@
-// firebase kütüphanesinden her şeyi import
-//ettik "firebase" objesi olarak
-import * as firebase from "firebase"
-
-//firebase projemize bağlanmamız için firebase'in
-//verdiği config objesini yapıştırıyoruz.
-var config = {
-  
-}
-
-//Diğer class yani componentlerde kullanabilmek, import edebilmek
-//için export etmemiz lazım
+// firebase kütüphanesinden her şeyi import ettik "firebase" objesi olarak
+import * as firebase from 'firebase';
+// firebase projemize bağlanmamız için firebase'in
+// verdiği config objesini yapıştırıyoruz.
+const config = {};
+// Diğer class yani componentlerde kullanabilmek, import edebilmek
+// için export etmemiz lazım
 export default (!firebase.apps.length
   ? firebase.initializeApp(config)
-  : firebase.app())
+  : firebase.app());
